6613aa1ee007fa4d6819abcf9e72e3c7b1793203
Ayush Saini <101219576+Ayushsaini076@users.noreply.github.com>
2023-05-11 00:18:26 +0530

    second commit of backend

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const bodyParser = require('body-parser')
 const cors = require('cors');
+const mongoose = require('mongoose');
 
 const MongoClient = require('mongodb').MongoClient;
 
-const url = '';
+const url = 'mongodb://localhost:27017';
+const dbName='childcare';
+
+const dataSchema = new mongoose.Schema({
+    name:{type:String,required:true},
+    age:{type:Number,required:true}
+})
+
+const Data = mongoose.model('Data',dataSchema);
+
+
 
 const app = express();
 app.use(cors());
@@ -13,13 +24,24 @@ app.use(bodyParser.json());
 
 const port =3001;
 
+app.get('/',async (req,res)=>{
+    try{
+        const data = await Data.find();
+        res.json(data);
+    }
+    catch(err){
+        res.status(500).send(err);
+    }
+    
+})
+
 app.post('/', async (req, res) => {
     const data = req.body;
   
       const client = new  MongoClient(url, {useNewUrlParser:true});
       client.connect();
       const db = client.db(dbName);
-      const collection = db.collection('registrations');
+      const collection = db.collection('child-data');
   
       await collection.insertOne(data);
       console.log("inserted successfully")
